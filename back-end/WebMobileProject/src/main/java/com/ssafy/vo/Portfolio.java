02498ae38803cbfff30f4102c778cb365e3e7a19
woogie91 <woogie91@naver.com>
2019-07-25 15:29:12 +0900

    코드정리

--- back-end/WebMobileProject/src/main/java/com/ssafy/vo/Portfolio.java
+++ back-end/WebMobileProject/src/main/java/com/ssafy/vo/Portfolio.java
@@ -1,11 +1,11 @@
 package com.ssafy.vo;
 
-import java.util.ArrayList;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-/**
- * @author multicampus
- *
- */
+@Builder
+@Getter @Setter
 public class Portfolio {
 	private String pfId;
 	private String title;
@@ -14,81 +14,4 @@ public class Portfolio {
 	private String date;
 	private String img;
 	private String name;
-	
-	public Portfolio() {}
-
-	public Portfolio(String pfId, String title, String content, String count, String date, String img, String name) {
-		this.pfId = pfId;
-		this.title = title;
-		this.content = content;
-		this.count = count;
-		this.date = date;
-		this.img = img;
-		this.name = name;
-	}
-
-	public String getPfId() {
-		return pfId;
-	}
-
-	public void setPfId(String pfId) {
-		this.pfId = pfId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getCount() {
-		return count;
-	}
-
-	public void setCount(String count) {
-		this.count = count;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Portfolio [pfId=" + pfId + ", title=" + title + ", content=" + content + ", count=" + count + ", date="
-				+ date + ", img=" + img + ", name=" + name + "]";
-	}
-
-	
-	
 }
--- back-end/WebMobileProject/src/main/java/com/ssafy/vo/Post.java
+++ back-end/WebMobileProject/src/main/java/com/ssafy/vo/Post.java
@@ -1,7 +1,11 @@
 package com.ssafy.vo;
 
-import java.util.ArrayList;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Builder
+@Getter @Setter
 public class Post {
 	private String postId;
 	private String title;
@@ -10,82 +14,4 @@ public class Post {
 	private String date;
 	private String img;
 	private String name;
-	
-	public Post() {}
-
-	public Post(String postId, String title, String content, String count, String date, String img, String name) {
-		super();
-		this.postId = postId;
-		this.title = title;
-		this.content = content;
-		this.count = count;
-		this.date = date;
-		this.img = img;
-		this.name = name;
-	}
-
-	public String getPostId() {
-		return postId;
-	}
-
-	public void setPostId(String postId) {
-		this.postId = postId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getCount() {
-		return count;
-	}
-
-	public void setCount(String count) {
-		this.count = count;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Post [postId=" + postId + ", title=" + title + ", content=" + content + ", count=" + count + ", date="
-				+ date + ", img=" + img + ", name=" + name + "]";
-	}
-
-	
-	
 }
