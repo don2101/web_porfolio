9cf9164bb0afcd33e486d17808d9c73c5e6b2064
don2101 <don210198@gmail.com>
2019-07-25 15:56:07 +0900

    S1P1113033-11 PostService 메서드 추가
    - postlist, post를 가져오는 메서드
    - post를 올리는 메서드
    - post를 삭제하고, 수정하는 메서드 추가

---
+++ front-end/src/service/PostService.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:9090/post"
+
+export default {
+  async getPostList() {
+    const resposne = await axios.get(BASE_URL);
+
+
+  },
+
+  async postPost(postBody) {
+    const response = await axios.post(BASE_URL, postBody);
+
+  },
+
+  async getPost(postId) {
+    const response = await axios.get(BASE_URL + "/" + postId);
+
+  },
+
+  async putPost(postId, postBody) {
+    const response = await axios.put(BASE_URL + "/" + postId, postBody);
+
+  },
+
+  async deletePost(postId) {
+    const response = await axios.delete(BASE_URL + "/" + postId);
+  }
+
+}
