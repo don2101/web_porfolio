1934758b1869b0d9105fa8ad1294947810e4887b
don2101 <don210198@gmail.com>
2019-08-08 17:33:48 +0900

    S1P1113033-42 portfolio write title 간격 조정

--- front-end/src/views/portfolios/PortfolioWrite.vue
+++ front-end/src/views/portfolios/PortfolioWrite.vue
@@ -1,57 +1,58 @@
 <template>
   <div>
-    <div class="my-5">
+      
+    <form @submit.prevent="postPortfolio">
+
       <!-- title -->
-      <form @submit.prevent="postPortfolio">
-        <v-layout>
-          <v-flex xs12>
-            <v-text-field
-            v-model="title"
-            dark
-            color="#FAFAFA"
-            outline single-line
-            height="50" style="font-size: 30px" name="title" required></v-text-field>
-          </v-flex>
-        </v-layout>
+      <v-layout class="portfolio-title-input">
+        <v-flex xs12>
+          <v-text-field
+          v-model="title"
+          dark
+          color="#FAFAFA"
+          outline single-line
+          height="50" style="font-size: 30px" name="title" required></v-text-field>
+        </v-flex>
+      </v-layout>
 
-        <v-layout>
-          <!-- text view area -->
-          <v-flex xs6 class="mr-3">
-            <img id="image" v-bind:src="imageSource" style="max-width: 100%">
+      <v-layout class="portfolio-body-input">
+        <!-- text view area -->
+        <v-flex xs6 class="mr-3">
+          <img id="image" v-bind:src="imageSource" style="max-width: 100%">
 
-            <div id="registedImages">
-              <v-textarea
-                background-color="transparent" color="#FAFAFA"
-                auto-grow dark solo readonly flat
-                v-bind:value="content">
-                {{ imageSource }}
-              </v-textarea>
-            </div>
-          </v-flex>
-
-          <!-- text area -->
-          <v-flex xs6>
-            <imageUploader @passUploadImage="setImageSource" />
-
+          <div id="registedImages">
             <v-textarea
-              class="my-3" color="#FAFAFA"
-              outline dark auto-grow flat
-              v-model="content" name="content" required>
-
+              background-color="transparent" color="#FAFAFA"
+              auto-grow dark solo readonly flat
+              v-bind:value="content">
+              {{ imageSource }}
             </v-textarea>
+          </div>
+        </v-flex>
 
-            <!-- submit button -->
-            <v-btn
-              :class="{'red-color': this.buttonPicked}" color="#FAFAFA"
-              flat outline>
-              <input type="submit" value="WRITE" @mouseover="buttonPick" @mouseleave="buttonPick">
+        <!-- text area -->
+        <v-flex xs6>
+          <imageUploader @passUploadImage="setImageSource" />
 
-            </v-btn>
+          <v-textarea
+            class="my-3" color="#FAFAFA"
+            outline dark auto-grow flat
+            v-model="content" name="content" required>
 
-          </v-flex>
-        </v-layout>
-      </form>
-    </div>
+          </v-textarea>
+
+          <!-- submit button -->
+          <v-btn
+            :class="{'red-color': this.buttonPicked}" color="#FAFAFA"
+            flat outline>
+            <input type="submit" value="WRITE" @mouseover="buttonPick" @mouseleave="buttonPick">
+
+          </v-btn>
+
+        </v-flex>
+      </v-layout>
+    </form>
+
   </div>
 </template>
 
