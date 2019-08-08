f19c9686214aceda5736cfbeee6e91fd8bd00471
don2101 <don210198@gmail.com>
2019-08-08 10:47:25 +0900

    gitlab url 오류 수정

--- front-end/src/service/GitlabApiService.js
+++ front-end/src/service/GitlabApiService.js
@@ -4,8 +4,8 @@
 
 import axios from 'axios'
 
-const GITLAB_URL_1 = "https://lab.ssafy.com/api/v4/projects/6033/repository/commits?&all=true&per_page=500"
-const GITLAB_URL_2 = "https://lab.ssafy.com/api/v4/projects/6033/repository/commits?&all=true&per_page=500"
+const GITLAB_URL = "https://lab.ssafy.com/api/v4/projects/6033/repository/commits?&all=true&per_page=500"
+// const GITLAB_URL_2 = "https://lab.ssafy.com/api/v4/projects/6033/repository/commits?&all=true&per_page=500"
 
 
 export default {
