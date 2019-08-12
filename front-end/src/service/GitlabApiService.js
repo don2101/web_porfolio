5d5423d60e43d059130cfb6a87bc1528b4099841
don2101 <don210198@gmail.com>
2019-08-12 09:43:55 +0900

    S1P1113033-41 AboutUs 페이지 변경
    - title 간격 조정
    - gitlab에서 commit data를 가져와 추가

--- front-end/src/service/GitlabApiService.js
+++ front-end/src/service/GitlabApiService.js
@@ -5,6 +5,7 @@
 import axios from 'axios'
 
 const GITLAB_URL = "https://lab.ssafy.com/api/v4/projects/6033/repository/commits?&all=true&per_page=500"
+const GITLAB_URL2 = "https://lab.ssafy.com/api/v4/projects/7498/repository/commits?&all=true&per_page=500"
 
 
 
@@ -32,13 +33,35 @@ export default {
 
   // api데이터에서 committer_name, committed_date 추츨
   async apiRequest() {
-    const parseData = {}
-    const response = await axios.get(GITLAB_URL, {
+    let parseData = {}
+    let response = await axios.get(GITLAB_URL, {
         headers: {
           "PRIVATE-TOKEN": "G_sSKz7HmcoLts8nbC74"
         }
+      })
+
+    for(let i = 0; i < response.data.length; i++) {
+      let committerId = response.data[i]['committer_name']
+      let commiterName = this.name[committerId]
+      let commitDate = response.data[i]['committed_date']
+
+      if(this.isExist(parseData[commiterName])) {
+        parseData[commiterName].push(
+          this.parseDate(commitDate)[0]
+        )
+      } else {
+        parseData[commiterName] = []
+        parseData[commiterName].push(
+          this.parseDate(commitDate)[0]
+        )
       }
-    )
+    }
+
+    response = await axios.get(GITLAB_URL2, {
+      headers: {
+        "PRIVATE-TOKEN": "G_sSKz7HmcoLts8nbC74"
+      }
+    })
 
     for(let i = 0; i < response.data.length; i++) {
       let committerId = response.data[i]['committer_name']
